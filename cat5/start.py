eda3a60459caf5eb646b810358752abb7bd34e24
Jack Arnold <42704019+jackarnold84@users.noreply.github.com>
2025-01-26 16:44:38 -0600

    Better player model (#3)
    * better player stat projection
    
    * empty player slot
    
    * fix emptyplayer signature
    
    * fix default iter power

--- cat5/start.py
+++ cat5/start.py
@@ -1,15 +1,11 @@
 
 from datetime import date, datetime
 from functools import cache
+from typing import Dict, List, TypeAlias
 
 from espn_api.basketball import Player
 
-cat_to_weight = {
-    'FT%': 25,
-    'FG%': 20,
-    'STL': 20,
-    'BLK': 20,
-}
+PlayerStats: TypeAlias = Dict[str, Dict[str, Dict[str, float]]]
 
 
 class PlayerStart:
@@ -19,23 +15,67 @@ class PlayerStart:
         self.game_datetime: datetime = player.schedule[game_day_id]['date']
         self.game_date: date = self.game_datetime.date()
         self.injured = self.player.injured
+        self.ema_window = 30
 
     def __repr__(self) -> str:
         return f'Start({self.player}, Date({self.game_date}))'
 
     @cache
     def projection(self, cat: str) -> float:
-        stats = self.player.stats
-        # TODO: weight with last 7/15/30 stats
-        preseason_stats = stats[f'{self.player.year}_projected']
-        actual_stats = stats[f'{self.player.year}_total']
+        player_stats: PlayerStats = self.player.stats
+        year = self.player.year
+        preseason_avg = player_stats.get(f'{year}_projected', {}) \
+            .get('avg', {}).get(cat, 0)
 
-        preseason = preseason_stats['avg'].get(cat, 0)
-        actual = actual_stats['avg'].get(cat, 0)
-        actual_gp = actual_stats['total']['GP']
+        periods = ['last_7', 'last_15', 'last_30', 'total']
+        stats = {
+            period: player_stats.get(f'{year}_{period}', {}) for period in periods
+        }
+        gps = {period: int(stats[period].get('total', {}).get('GP', 0))
+               for period in periods}
+        avgs = {period: stats[period].get('avg', {}).get(cat, 0)
+                for period in periods}
+        inside_gps = {
+            period: gps[period] - gps[periods[i-1]]
+            for i, period in enumerate(periods) if i > 0
+        }
+        inside_avgs = {
+            period: (avgs[period] * gps[period] - avgs[periods[i-1]] * gps[periods[i-1]]) /
+            max(inside_gps[period], 1)
+            for i, period in enumerate(periods) if i > 0
+        }
 
-        a = cat_to_weight.get(cat, 15)
-        d = 3
-        w_pre = a**d
-        w_act = actual_gp**d
-        return (w_pre * preseason + w_act * actual) / (w_pre + w_act)
+        ts: List[float] = (
+            [avgs['last_7']] * gps['last_7'] +
+            [inside_avgs['last_15']] * inside_gps['last_15'] +
+            [inside_avgs['last_30']] * inside_gps['last_30'] +
+            [avgs['total']] * gps['total']
+        )
+        ts += [preseason_avg] * (self.ema_window - len(ts))
+        ts = list(reversed(ts))
+        return ema_next(ts)
+
+
+class EmptyStart(PlayerStart):
+    class EmptyPlayer(Player):
+        def __init__(self):
+            self.playerId = 0
+            self.name = 'Empty'
+            self.position = 'X'
+            self.proTeam = 'X'
+            self.percent_owned = 0.0
+
+    def __init__(self):
+        self.player = self.EmptyPlayer()
+
+    @cache
+    def projection(self, _: str) -> float:
+        return 0.0
+
+
+def ema_next(ts: List[float]) -> float:
+    alpha = 2 / (len(ts) + 1)
+    ma = ts[0]
+    for t in ts[1:]:
+        ma = alpha * t + (1 - alpha) * ma
+    return ma
